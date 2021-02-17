9f90834958cf77de9382dc5815c8a40ffd33fcfa
Erik Bard <erik.bard@outlook.com>
2021-02-17 16:17:14 +0100

    Add project files.

---
+++ Väderdata.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Väderdata.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
