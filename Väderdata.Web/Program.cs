4c12ff3e61253cb102df56f7e92158a0d0dc1035
Erik Bard <erik.bard@outlook.com>
2021-02-17 18:01:30 +0100

    edit

--- Väderdata.Web/Program.cs
+++ Väderdata.Web/Program.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Väderdata.Web.Context;
 
 namespace Väderdata.Web
 {
@@ -14,6 +16,18 @@ namespace Väderdata.Web
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+
+            ////////// --> TESTING IN PROGRESS!!! <--- ///////////////
+
+            // read data from csv and store in datatable:
+            DataTable datatable = CsvReader.GetDataTabletFromCSVFile();
+            // insert from datatable into sql-table:
+            CsvReader.InsertDataAsBulk(datatable);
+
+            /////////////////////////////////////////////////////////
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
