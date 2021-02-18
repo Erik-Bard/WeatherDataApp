200adc6f151539ea8f587d0de7deb070f97d6711
Marcus Thenander <marcus.thenander13@gmail.com>
2021-02-18 09:57:22 +0100

    edit to info

--- Väderdata.Web/Information/ReglerFörHöstVinterMögel.cs
+++ Väderdata.Web/Information/ReglerFörHöstVinterMögel.cs
@@ -16,18 +16,43 @@ namespace Väderdata.Web
            definitionen av de olika årstiderna
         -- Som ytterligare villkor gäller att hösten inte kan börja före den 1 augusti. Det är någon vecka efter medeldatum för årets varmaste dag.
 
+
         Vinter
         -- Meteorologer definierar vinter som den period då dygnets medeltemperatur varaktigt är 0,0 grader eller lägre.
         -- Om dygnsmedeltemperaturen är 0,0°C eller lägre fem dygn i följd, säger vi att vintern anlände det första av dessa dygn.
         -- Även om det blir en återgång till högre temperaturer därefter så räknas det fortfarande som vinter.
         -- I vår definition av höstens ankomst har vi satt den 1 augusti som tidigast möjliga datum och naturligtvis kan inte heller vintern komma tidigare än så.
-        -- I det svenska klimatet är det praktiskt taget alltid höst under en längre eller kortare period innan vintern anländer. Men skulle villkoret för vinter uppfyllas innan eller samtidigt som villkoret för höst är uppfyllt, så skulle det rent teoretiskt kunna bli vinter utan föregående höst.
+        -- I det svenska klimatet är det praktiskt taget alltid höst under en längre eller kortare period innan vintern anländer. Men skulle villkoret för vinter uppfyllas innan 
+            eller samtidigt som villkoret för höst är uppfyllt, så skulle det rent teoretiskt kunna bli vinter utan föregående höst.
         -- I enstaka fall har det  inträffat att villkoret för vinter uppfyllts först när våren redan har anlänt. I så fall tillåts inte årstiderna att backa utan det förblir vår.
-        -- En beräkning av dygnsmedeltemperaturen kan resultera i ett antal decimaler, men man gör en avrundning till en tiondels grad innan man tillämpar definitionen av de olika årstiderna. Exempelvis avrundas 0,02° till 0,0°.
+        -- En beräkning av dygnsmedeltemperaturen kan resultera i ett antal decimaler, men man gör en avrundning till en tiondels grad innan man tillämpar definitionen av de 
+            olika årstiderna. Exempelvis avrundas 0,02° till 0,0°.
          
+
         Mögel Risk Definition
-        -- 
+        --  http://www.penthon.com/vanliga-fragor/faq/vad-innebar-mogelindex/
+        -- temperatur från 0 till 50 grader
+        -- fukt under 78% för torrt
+        -- Python formula för fukt
+
+        rtemp = relative temperature
+        rhum = relative humidity
+
+
+                rtemp = round(temp)
+                rhum = round(hum)
+                if (rtemp <= 0) or (rtemp > 50):
+                     mindex = 0
+                else:
+                     for i in range(1,4):
+                          if (rhum < mtab[rtemp][i]):
+                               mindex = i-1
+                               break
+                     else:
+                          mindex = 3
+                print (mindex)
 
          */
+
     }
 }
---
+++ Väderdata.Web/Information/ToDoProject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Väderdata.Web.Information
+{
+    public class ToDoProject
+    {
+        /*
+         G
+
+
+            Följande information skall kunna visas
+
+            • Utomhus:
+
+            ▫ Medeltemperatur för valt datum (sökmöjlighet)
+            ▫ Sortering av varmaste till kallaste dagen enligt medeltemperatur per dag
+            ▫ Sortering av torraste till fuktigaste dagen enligt medelluftfuktighet per dag
+            ▫ Sortering av minst till störst risk av mögel
+            ▫ Datum för meteorologisk Höst
+            ▫ Datum för meteorologisk Vinter (OBS! Vintern 2016 var mild)
+
+            • Inomhus:
+
+            ▫ Medeltemperatur för valt datum (sökmöjlighet)
+            ▫ Sortering av varmaste till kallaste dagen enligt medeltemperatur per dag
+            ▫ Sortering av torraste till fuktigaste dagen enligt medelluftfuktighet per dag
+            ▫ Sortering av minst till störst risk av mögel
+
+
+            Frågor som kan komma
+
+            • Val av datatyper i DB för den här typen av data.
+            • Inläsning av textfil till databasen
+            • Algoritmer som räknar fram aggregerad data
+            • Meteorologiska regler för Höst, Vinter och Mögelindex
+            • Mögelrisk, hitta formel.
+            • Källkoden dokumenterad i löpande kod, med särskild stor vikt vid 
+                    användandet av  algoritmer. Förklara dina val av algoritmer och 
+                    datastrukturer.
+            • Jämför gärna de resultat du får i din app med andra elever, för att dela 
+                    med er av hur  ni tänkt.
+            • Samtliga krav och specifikationer kan ändras under arbetets gång.
+         
+         
+
+        VG
+
+            ▫ Hur länge är balkongdörren öppen per dag, och sorterat på detta.
+                 Antagandet är att om balkongdörren öppnas så sjunker innertemperaturen lite.
+                 Yttertemperaturen höjs också lite grann, då termometern sitter nära 
+                balkongdörren.
+
+            ▫ Sortering på då inne- och yttertemperaturen skiljt sig mest och minst.
+
+         
+         
+         
+         
+         */
+    }
+}
