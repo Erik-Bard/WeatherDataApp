0a896f01771b8b5f8eb305faf1239ee9ff3e8faf
Maja Sköldstam <maja.skoldstam@enjoygroup.se>
2021-02-25 14:48:29 +0100

    Added comments

--- Väderdata.Web/Data/CsvModelClass.cs
+++ Väderdata.Web/Data/CsvModelClass.cs
@@ -8,6 +8,7 @@ namespace Väderdata.Web.Data
 {
     public class CsvModelClass
     {
+        //Här har vi låtit propertynamnen vara på svenska eftersom dom står på svenska i CSV-filen.
         public int Id { get; set; }
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime Datum { get; set; }
