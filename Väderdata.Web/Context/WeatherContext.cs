f26600dcde26efcbf6535c66641f0dc56a847338
Erik Bard <erik.bard@outlook.com>
2021-02-25 11:44:55 +0100

    Removed some unessesary classes.

--- Väderdata.Web/Context/WeatherContext.cs
+++ Väderdata.Web/Context/WeatherContext.cs
@@ -19,8 +19,10 @@ namespace Väderdata.Web.Context
         public DbSet<MögelRisk> MögelRisks { get; set; }
         public DbSet<MeteorologiskSäsong> WeatherSeason { get; set; }
         public DbSet<BalconyDoor> BalconyDoor { get; set; }
-        //--- Fetches ReadOnly data to use in avgTemp as Viewbag. --- //
-        public DbSet<ReadOnlyEnviroment> ReadOnlyEnv { get; set; }
+        public DbSet<InformationTableIndoor> InformationTable { get; set; }
+        public DbSet<InformationTableOutdoor> InformationTableOutdoor { get; set; }
+
+        // Modelbuilder for all entities that need specific mapping etc
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CsvModelClass>()
@@ -28,7 +30,5 @@ namespace Väderdata.Web.Context
             modelBuilder.Entity<MögelRisk>()
                 .ToTable("MögelRisk");
         }
-        public DbSet<Väderdata.Web.Data.InformationTableIndoor> InformationTable { get; set; }
-        public DbSet<Väderdata.Web.Data.InformationTableOutdoor> InformationTableOutdoor { get; set; }
     }
 }
